fd987f354ad3e1bbdf8f061182ee065c97ec3c2f
Jefferson Carlos <jeff.carlosbd@gmail.com>
2022-05-03 23:13:49 -0300

    cria MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import './App.css'
 
-const App: React.FC = () =>  {
+const App: React.FC = () => {
+  const {
+    listening,
+    transcript,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition()
+
+  const [number, setNumber] = useState(Math.floor(Math.random() * 10))
+  const [color, setColor] = useState('black')
+
+  useEffect(() => {
+    function updateNumber() {
+      setTimeout(() => {
+        resetTranscript()
+        setColor('black')
+        setNumber(Math.floor(Math.random() * 10))
+      }, 2000)
+    }
+
+    if (transcript && listening === false) {
+      if (transcript === String(number)) {
+        setColor('green')
+        updateNumber()
+      }
+      if (transcript !== String(number)) {
+        setColor('red')
+        updateNumber()
+      }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [transcript, listening])
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>
+  }
+
   return (
-    <div>
-      Oi
+    <div id="container">
+      <span style={{ color }}>{number}</span>
+      {listening ? (
+        <div onClick={() => SpeechRecognition.stopListening()}>❌</div>
+      ) : (
+        <div onClick={() => SpeechRecognition.startListening({ language: 'pt-BR' })}>🎙️</div>
+      )}
+      
     </div>
   )
 }
---
+++ src/numbers.ts
@@ -0,0 +1,12 @@
+export const numbers = [
+    { number: 0, text: 'zero' },
+    { number: 1, text: 'um' },
+    { number: 2, text: 'dois' },
+    { number: 3, text: 'tres' },
+    { number: 4, text: 'quatro' },
+    { number: 5, text: 'cinco' },
+    { number: 6, text: 'seis' },
+    { number: 7, text: 'sete' },
+    { number: 8, text: 'oito' },
+    { number: 9, text: 'nove' }
+]
